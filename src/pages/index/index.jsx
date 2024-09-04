d6b22d0985abf5a0927810b7d73ae0a07773dd6f
Oliver <olimovier@gmail.com>
2024-09-04 16:38:35 +0800

    授权手机号按钮

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,15 +1,25 @@
-import { View, Text } from '@tarojs/components'
+import { View, Button } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 
 export default function Index () {
+  
   useLoad(() => {
     console.log('Page loaded.')
   })
 
+  const getPhoneNumber = async e => {
+    const { code } = e.detail;
+    if (!code) {
+      console.log('用户拒绝授权手机号！！！');
+      return;
+    }
+    console.log(code);
+  };
+
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+      <Button openType='getPhoneNumber' onGetPhoneNumber={getPhoneNumber}>点击授权手机号</Button>
     </View>
   )
 }
